d20c7331650f1ed53d8cf0ce7a1fd61eea2d7efb
MuffinDev <92362765+MuffinDev9@users.noreply.github.com>
2021-11-19 10:04:38 +0000

    JHGF

--- files/scripts/script.js
+++ files/scripts/script.js
@@ -2,16 +2,24 @@
 
 // Changes Text
 function changeText() {
-	document.getElementById("textChange").innerHTML = "Now Click The Other One."
-	document.getElementById("1st").innerHTML = "Then Click Me!"
-	document.getElementById("2nd").innerHTML = "Click Me!"
-	console.log("You Pressed The Button.")
+	document.getElementById("textChange").innerHTML = "Now Click The Other One.";
+	document.getElementById("1st").innerHTML = "Then Click Me!";
+	document.getElementById("2nd").innerHTML = "Click Me!";
+	console.log("You Pressed The Button.");
 }
 
 // Changes Text Back
 function changeBack() {
-	document.getElementById("textChange").innerHTML = "Click The Button."
-	document.getElementById("1st").innerHTML = "Click Me!"
-	document.getElementById("2nd").innerHTML = "Then Click Me!"
-	console.log("You Pressed The Other Button.")
+	document.getElementById("textChange").innerHTML = "Click The Button.";
+	document.getElementById("1st").innerHTML = "Click Me!";
+	document.getElementById("2nd").innerHTML = "Then Click Me!";
+	console.log("You Pressed The Other Button.");
+}
+
+// Second Text(The One About Variables)
+
+// Button To Change Thing
+function changingVariables() {
+	var name = "My Name is Thomas!";
+	document.getElementById("ToVariable").innerHTML = name;
 }
